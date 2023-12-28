0ca1b3c6ea0a689c4931687321764f2241f3930a
kyleholgate <kyle.holgate@gmail.com>
2023-12-28 15:00:26 -0500

    sample messages WIP

--- app/components/forms/HL7ParserForm.tsx
+++ app/components/forms/HL7ParserForm.tsx
@@ -14,10 +14,15 @@ const HL7Parser = () => {
   const [text, setText] = useState('');
   const [outputs, setOutputs] = useState([]);
   const [isTipsVisible, setIsTipsVisible] = useState(false);
+  const [isSampleMessagesVisible, setIsSampleMessagesVisible] = useState(false);
   const [error, setError] = useState('');
 
   const toggleTips = () => {
     setIsTipsVisible(!isTipsVisible);
+  };
+
+  const toggleSampleMessages = () => {
+    setIsSampleMessagesVisible(!isSampleMessagesVisible);
   };
 
   const handleTextChange = (event) => {
@@ -54,6 +59,16 @@ const HL7Parser = () => {
           </>
         </Heading>
         {error && <div className='text-red-500 font-bold text-xl'>{error}</div>}
+        <div className='text-gray-500 pb-1'>or view <a className='underline' onClick={toggleSampleMessages}>sample messages</a></div>
+        <div className={`flex px-4 rounded transition-all duration-200 ease-in-out ${isSampleMessagesVisible ? 'py-1 max-h-screen mb-2' : 'py-0 max-h-0 border-none'} overflow-hidden text-foreground`}>
+          <div className="container mx-auto px-4 md:px-6 py-2">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-2 content-center justify-center">
+              <button className="w-full md:w-1/2 py-2 bg-gray-500 text-white rounded mx-auto">ADT</button>
+              <button className="w-full md:w-1/2 py-2 bg-gray-500 text-white rounded mx-auto">Lab</button>
+              <button className="w-full md:w-1/2 py-2 bg-gray-500 text-white rounded mx-auto">Pharmacy</button>
+            </div>
+          </div>
+        </div>
         <textarea
           id='hl7Input'
           className="w-full h-96 p-4 rounded text-lg ibm-plex-mono font-normal bg-background border-2 border-gray-300 text-foreground focus:border-foreground focus:outline-none focus:shadow-[0_0_5px_2px_rgba(0,0,0,0.5)]"
@@ -73,8 +88,8 @@ const HL7Parser = () => {
           <ul className='list-disc ps-4'>
             <li><strong>Hover</strong> over any field to see its definition</li>
             <li>Click on any field to <strong>copy it</strong></li>
-            <li>Click on the segment's <strong>expand icon</strong> <MdExpandMore class="inline" /> to see the full definition table</li>
-            <li>Click on the segment's <strong>copy icon</strong> <MdContentCopy class="inline" /> to copy the segment's raw value</li>
+            <li>Click on the segment's <strong>expand icon</strong> <MdExpandMore className="inline" /> to see the full definition table</li>
+            <li>Click on the segment's <strong>copy icon</strong> <MdContentCopy className="inline" /> to copy the segment's raw value</li>
           </ul>
         </div>
         <div id='parsedOutput' className="w-full overflow-x-auto whitespace-pre-wrap ibm-plex-mono divide-y-2 text-lg">{outputs}</div>
