cc5f72f925e9e3c5a1d746d38c579f795ac479b3
kyleholgate <kyle.holgate@gmail.com>
2024-01-03 21:47:28 -0500

    HL7 lookup

--- app/components/ui/Heading.tsx
+++ app/components/ui/Heading.tsx
@@ -8,26 +8,26 @@ interface HeadingProps {
 
 // where children is the text or React element to be displayed
 const Heading: React.FC<HeadingProps> = ({ level, children, ...rest }) => {
-    const baseStyle = "font-extrabold text-black tracking-tight leading-tight my-8";
+    const baseStyle = "font-extrabold text-black tracking-tight leading-tight";
     let HeadingTag = `h${level}`;
 
     let additionalStyle = "";
     switch (level) {
         case 1:
-            additionalStyle = "text-5xl";
+            additionalStyle = "text-5xl my-8";
             break;
         case 2:
-            additionalStyle = "text-4xl text-gray-800";
+            additionalStyle = "text-4xl text-gray-800 mt-6 mb-2";
             break;
         case 3:
-            additionalStyle = "text-3xl text-gray-700";
+            additionalStyle = "text-3xl text-gray-700 mt-4 mb-2";
             break;
         case 4:
-            additionalStyle = "text-2xl text-gray-600";
+            additionalStyle = "text-2xl text-gray-600 mt-4 mb-2";
             break;
         default:
             HeadingTag = `h2`;
-            additionalStyle = "text-4xl text-gray-800";
+            additionalStyle = "text-4xl text-gray-800 mt-6 mb-2";
     }
     const attributes = {
         className: `${baseStyle} ${additionalStyle}`,
--- app/hl7/lookup/page.tsx
+++ app/hl7/lookup/page.tsx
@@ -12,6 +12,7 @@ export default function Page() {
         <div className="text-lg">
             <Heading level={1}>HL7 Segment & Field Lookup</Heading>
             <SegmentDropdown />
+            <p className="text-xs">Want to search by description? Use Ctrl+F to search with your browser.</p>
             {Object.entries(hl7Fields).map(([segment, fields]) => (
                 <div key={segment}>
                     <Heading level={2} id={segment}>{segment}</Heading>
@@ -33,6 +34,14 @@ export default function Page() {
                     </table>
                 </div>
             ))}
+            <Heading level={2}>Segment Types in HL7</Heading>
+            <p>In HL7 messaging, data is organized into segments, each serving a specific purpose in the overall communication structure. A segment is a logical grouping of data fields that convey a particular set of related information. Each segment is identified by a unique segment type code, typically a three-letter abbreviation, which appears at the beginning of the segment line in a message.</p>
+
+            <Heading level={2}>Understanding Fields and Their Mappings</Heading>
+            <p>Each segment consists of multiple fields, which are individual pieces of information. Fields are usually separated by a delimiter, such as a pipe "|" character, and are identified by their position within the segment. The content and format of these fields are defined by the HL7 standards, and understanding their mappings is crucial for accurate data interpretation and integration.</p>
+
+            <Heading level={2}>What's an HL7 Lookup Table?</Heading>
+            <p>An HL7 lookup table is a reference tool that provides a detailed description of each segment type and its corresponding fields. It is a valuable resource for healthcare professionals and IT experts who need to understand the structure and content of HL7 messages. The lookup table also serves as a crosswalk between HL7 2.x and HL7 FHIR, which is the latest version of the HL7 standard.</p>
         </div>
     )
 }
