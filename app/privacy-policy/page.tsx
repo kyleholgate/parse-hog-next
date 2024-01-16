0d014371b4c8110ad4af33d4aa4ed72f04675dd8
kyleholgate <kyle.holgate@gmail.com>
2024-01-15 22:30:42 -0500

    privacy policy

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,90 @@
+import Heading from "@/app/components/ui/Heading";
+
+export const metadata = {
+    title: 'Privacy Policy',
+    description: '',
+}
+
+export default function Page() {
+    return (
+        <div className="text-lg">
+            Privacy Policy for ParseHog.com
+
+            Last Updated: January 15, 2024
+
+            1. Introduction
+
+            Welcome to ParseHog.com. We are committed to safeguarding the privacy of our website visitors and service users. This policy applies where we are acting as a data controller with respect to the personal data of our website visitors and service users; in other words, where we determine the purposes and means of the processing of that personal data.
+
+            2. Credit
+
+            This document was created using a template from SEQ Legal (seqlegal.com) and modified by ParseHog.com.
+
+            3. Collecting Personal Information
+
+            The following types of personal information may be collected, stored, and used:
+
+            Information about your computer including your IP address, geographical location, browser type and version, and operating system.
+            Information about your visits to and use of this website including the referral source, length of visit, page views, and website navigation paths.
+            Information that you enter when you register with our website, such as your email address.
+            Information that you enter in order to set up subscription to our emails and/or newsletters.
+            Information that is generated while using our website, including when, how often, and under what circumstances you use it.
+            Any other personal information that you send to us.
+
+            4. Using Personal Information
+
+            Personal information submitted to us through our website will be used for the purposes specified in this policy or on the relevant pages of the website. We may use your personal information for the following:
+
+            Administering our website and business.
+            Enabling your use of the services available on our website.
+            Keeping our website secure and prevent fraud.
+            Verifying compliance with the terms and conditions governing the use of our website.
+
+            5. Disclosing Personal Information
+
+            We may disclose your personal information to any of our employees, officers, insurers, professional advisers, agents, suppliers, or subcontractors as reasonably necessary for the purposes set out in this policy.
+
+            We may disclose your personal information:
+
+            To the extent that we are required to do so by law.
+            In connection with any ongoing or prospective legal proceedings.
+            In order to establish, exercise, or defend our legal rights (including providing information to others for the purposes of fraud prevention and reducing credit risk).
+            To any person who we reasonably believe may apply to a court or other competent authority for disclosure of that personal information where, in our reasonable opinion, such court or authority would be reasonably likely to order disclosure of that personal information.
+
+            6. International Data Transfers
+
+            Information that we collect may be stored and processed in and transferred between any of the countries in which we operate in order to enable us to use the information in accordance with this policy.
+
+            7. Security of Personal Information
+
+            We will take reasonable technical and organizational precautions to prevent the loss, misuse, or alteration of your personal information.
+
+            8. Amendments
+
+            We may update this policy from time to time by publishing a new version on our website. You should check this page occasionally to ensure you understand any changes to this policy.
+
+            9. Your Rights
+
+            You may instruct us to provide you with any personal information we hold about you; provision of such information will be subject to the following:
+
+            The payment of a fee [if legally permissible].
+            The supply of appropriate evidence of your identity.
+
+            10. Third Party Websites
+
+            Our website includes hyperlinks to, and details of, third party websites. We have no control over, and are not responsible for, the privacy policies and practices of third parties.
+
+            11. Updating Information
+
+            Please let us know if the personal information that we hold about you needs to be corrected or updated.
+
+            12. Cookies
+
+            Our website uses cookies. A cookie is a file containing an identifier (a string of letters and numbers) that is sent by a web server to a web browser and is stored by the browser. The identifier is then sent back to the server each time the browser requests a page from the server. Cookies may be either "persistent" cookies or "session" cookies: a persistent cookie will be stored by a web browser and will remain valid until its set expiry date, unless deleted by the user before the expiry date; a session cookie, on the other hand, will expire at the end of the user session, when the web browser is closed.
+
+            13. Contact Us
+
+            If you have any questions about this privacy policy or our treatment of your personal information, please write to us by email.
+        </div>
+    )
+}
